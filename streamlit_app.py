f709c0e178cefffc2a20746ab73434a6b1207a67
jiofenuipoa <r.y.a.pgee@gmail.com>
2026-02-11 14:13:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ name = st.text_input("Как се казваш?")
 if name:
   st.write("Здравей,", name)
 
-age = st.number_input("На колко години си?", age)
+st.number_input("На колко години си?", age)
 
 if age < 18:
   st.write(f"Ти си на {age} години и НЕ си пълнолетен!")
